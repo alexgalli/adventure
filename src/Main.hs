57e777023c8a90eb3a70de87851e4d71925c2014
Alex Gallichotte <agallichotte@nationalresearch.com>
2014-11-20 18:07:33 -0800

    New: add file persistence

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,46 @@
+import System.Directory
+import System.IO
+
 main :: IO ()
 main = greet
 
-data Creature = Player {
+data Creature = Creature {
     name :: String
-}
+} deriving (Read, Show)
 
-
-getPlayer :: IO Creature
-getPlayer = do
+getCreature :: IO Creature
+getCreature = do
     putStrLn "what is your name? "
     playerName <- getLine
-    return (Player playerName)
+    return (Creature playerName)
 
+writeCreature :: String -> Creature -> IO ()
+writeCreature filename creature = do
+    handle <- openFile filename WriteMode
+    hPrint handle creature
+    hClose handle
+
+loadCreature :: String -> IO Creature
+loadCreature filename = do
+    handle <- openFile filename ReadMode
+    playerLine <- hGetLine handle
+    let player = read playerLine
+    hClose handle
+    return player
+
+datafile :: String
+datafile = "data.txt"
 
 greet :: IO ()
 greet = do
     putStrLn "hello world"
-    player <- getPlayer
+    isFile <- doesFileExist datafile
+    player <- if isFile
+        then
+            loadCreature datafile
+        else
+            getCreature
     putStrLn ("Hello, " ++ name player)
+    writeCreature datafile player
     putStrLn "bye!"
 
