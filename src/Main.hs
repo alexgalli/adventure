b0e35c17ed5b241c5ca91ca78a682c759305b827
Alex Gallichotte <agallichotte@nationalresearch.com>
2014-11-22 12:33:54 -0800

    New: now you can run a menu

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,8 @@
+import Control.Exception
+import Data.List
 import System.Directory
 import System.IO
+import Text.Read
 
 -- Creature handling
 data Creature = Creature {
@@ -40,32 +43,59 @@ getPlayer = do
     writeCreature datafile player
     return player
 
--- main menu
+-- menu
 
-newtype Menu = Menu [(String, IO ())]
+type MenuItem     = (String, IO ())
+type MenuListItem = (Integer, MenuItem)
+type Menu         = [MenuListItem]
+
+getMenu :: [MenuItem] -> Menu
+getMenu = zip [1..]
+
+showItem :: MenuListItem -> String
+showItem (ix, (text, _)) = show ix ++ ". " ++ text
+
+showMenu :: Menu -> String
+showMenu menu = intercalate "\n" $ map showItem menu
+
+printMenu :: Menu -> IO ()
+printMenu menu = putStrLn $ showMenu menu
+
+getItem :: Maybe Int -> Menu -> Maybe MenuItem
+getItem Nothing _ = Nothing
+getItem (Just ix) menu
+    | ix <= 0 = Nothing
+    | ix > length menu = Nothing
+    | otherwise = Just (snd (menu !! (ix - 1)))
+
+runMenu :: Menu -> IO ()
+runMenu menu = do
+    printMenu menu
+    let count = length menu
+    if count > 1
+        then putStr $ "Choose [1-" ++ show count ++ "]: "
+        else putStr "Choose [1]: "
+    ln <- getLine
+    case getItem (readMaybe ln) menu of
+        Just li -> do
+            putStrLn $ "You chose " ++ fst li
+            snd li
+        Nothing -> do
+            putStrLn $ "'" ++ ln ++ "' is not a valid selection."
+            runMenu menu
+
+-- main menu
 
 greet :: IO ()
 greet = putStrLn "hello world"
 
 mainMenu :: Menu
-mainMenu = Menu
-    [ ("Greet me", greet ) ]
+mainMenu = getMenu [ ("Greet me", greet ) ]
 
-getMenuLabels :: Menu -> [(Integer, String)]
-getMenuLabels (Menu menu) = zip [1..] (map fst menu)
-
-printMenu :: Menu -> IO ()
-printMenu menu = do
-    let labels = getMenuLabels menu
-    let stringLabels = map (\label -> show (fst label) ++ ". " ++ snd label) labels
-    mapM_ putStrLn stringLabels
-
-
--- main
 main :: IO ()
 main = do
     player <- getPlayer
     putStrLn ("Hello, " ++ name player)
-    printMenu mainMenu
+    runMenu mainMenu
     putStrLn "bye!"
 
